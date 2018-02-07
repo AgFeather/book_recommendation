06dcaac75339fccb636b37da23a6fde4f0b6ffb5
Li Dongfang <18843740508@163.com>
2018-02-07 19:47:58 +0900

    add data processing, data download module

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # book_recommendation
 This is a book recommendation system with tensorflow and python3.6
+
+## DataSet
+数据集使用的是goodbooks-10k数据集
+该数据集包含了对10k最受欢迎的数据的六百万条评论，同时也包括了：
+- books marked to read by the users
+- book metadata(author, year, etc.)
+- tags/shelves/genres
+数据源来自：https://github.com/zygmuntz/goodbooks-10k
+由于数据集过大，无法置于GitHub，所以全部数据集需要在以下link下载：
+https://github.com/zygmuntz/goodbooks-10k/releases
+
+href="https://github.com/zygmuntz/goodbooks-10k/releases/download/v1.0/goodbooks-10k.zip"
+
+### DataSet Contents
+**ratings.csv** contains ratings sored by time.(user_id, book_id, rating)
+Ratings go from one to five. Both book IDs and user IDs are contiguous. For books, they are 1-10000, for users, 1-53424.
+
+**to_read.csv** provids IDs of books marked "to read" by each user, sorted by time, There are close to a million pairs
+
+**books.csv** has metadata for each book (goodreads IDs, authors, title, average rating, etc) The metadata have been extracted from goodreads XML file, availiable in books_xml
+
+**book_tags.csv** contains tags/shelves/genres assigned by users to books. Tags in this file are represented by their IDs, They are sorted by goodreads_book_id ascending and count descending.
+Here, each tag/shelf is given an ID, tags.csv translates tag IDs to names
+
+**goodreads IDs** Each book may have many edition. goodreads_book_id and best_book_id generally point to the most popular edition of a  given book, while goodreads work id refers to the book in the abstract sense.
---
+++ data_download.py
@@ -0,0 +1,60 @@
+import zipfile
+import os
+from urllib.request import urlretrieve
+
+
+#下载数据集并解压
+def data_download():
+	url="https://github.com/zygmuntz/goodbooks-10k/releases/download/v1.0/goodbooks-10k.zip"
+	save_path = './'
+	data_name = 'goodbooks-10k.zip'
+	if os.path.exists(save_path+data_name):
+		print('found {} data'.format(data_name))
+	else:
+		with DLProgress(unit='B', unit_scale=True, miniters=1, desc='Downloading {}'.format(data_name)) as pbar:
+			urlretrieve(url, save_path, pbar.hook)
+
+
+def data_extract():
+	data_path = './goodbooks-10k.zip'
+	unzip_path = './data'
+	data_name = 'goodbooks-10k.zip'
+	extract_function = unzip
+	if os.path.exists(unzip_path):
+		os.makedirs(unzip_path)
+	try:
+		extract_function(data_name, data_path, unzip_path)
+	except Exception as err:
+		shutil.rmtree(unzip_path)
+		raise err
+	print('extracting done')
+
+
+def unzip(data_name, from_path, to_path):
+	print('extracting {} ...'.format(data_name))
+	with zipfile.ZipFile(from_path) as zf:
+		zf.extractall(to_path)
+
+
+class DLProgress(tqdm):
+	"""Handle progress bar while downloading"""
+	last_block = 0
+	def hook(self, block_num=1, block_size=1, total_size=None):
+		"""
+		a hook function that will be  called once on establishment
+		of the network connection and once after each block read
+		thereafter parameter:
+		block_num: acount of blocks transferred so far
+		block_size:block size in bytes
+		total_size: total size of the file. this may be -1 on older
+		FTP servers with do not return a file size in response to 
+		retrieval reqeust
+		"""
+		self.total = total_size
+		self.update((block_num - self.last_block) * block_size)
+		self.last_block = block_num
+		
+
+if __name__ == '__main__':
+	data_download()
+	data_extract()
